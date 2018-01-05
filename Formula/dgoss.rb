d625e96e336d56022505fb6a69a9bf978bfa5053
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-01-05 15:35:10 +0000

    Added dgoss formula.

---
+++ Formula/dgoss.rb
@@ -0,0 +1,32 @@
+require 'pp'
+
+class Dgoss < Formula
+  desc "Convenience wrapper for goss that simplifies Docker container testing"
+  homepage "https://goss.rocks"
+  url "https://github.com/aelsabbahy/goss/releases/download/v0.3.5/dgoss"
+  sha256 "288f5993fb25ec99b756cf9b93ebe6d79d757987255fc1ff15470f16fabf05ea"
+
+  bottle :unneeded
+
+  resource "goss" do
+    url "https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64"
+    sha256 "5669df08e406abf594de0e7a7718ef389e5dc7cc76905e7f6f64711e6aad7fa3"
+  end
+
+  def install
+    resource("goss").stage {
+      mv "goss-linux-amd64", "goss"
+      lib.install "goss"
+    }
+    pp "#{lib}"
+
+    # Update GOSS_PATH to point to the download goss binary.
+    inreplace "dgoss", /^GOSS_PATH=.*/, "GOSS_PATH=\"#{lib}/goss\""
+
+    bin.install "dgoss"
+  end
+
+  test do
+    system "#{bin}/dgoss", "--version"
+  end
+end

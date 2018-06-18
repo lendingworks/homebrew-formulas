b02c70f7c1a87a1bfc499a9b2ab16b6d906a49d2
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-06-18 09:58:25 +0100

    Upgraded dinghy and unfs3.

--- Formula/unfs3.rb
+++ Formula/unfs3.rb
@@ -1,19 +1,31 @@
 class Unfs3 < Formula
   desc "User-space NFSv3 server"
-  homepage "http://unfs3.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/unfs3/unfs3/0.9.22/unfs3-0.9.22.tar.gz"
-  sha256 "482222cae541172c155cd5dc9c2199763a6454b0c5c0619102d8143bb19fdf1c"
+  homepage "https://unfs3.sourceforge.io"
+
+  # Temporarily using Karl Mikaelsson's branch for nanosecond timestamps
+  # on Mac OS High Sierra until such time as he makes an official release.
+  # See: https://github.com/codekitchen/dinghy/issues/269#issuecomment-391007703
+  url "https://github.com/derfian/unfs3.git", :branch => "higher-resolution-setattr"
+  version "0.9.23-pre"
+  depends_on "autoconf"
+  depends_on "automake"
+
+  # Temporarily keeping 0.9.22 release as 'devel' to ease switching back to test
+  devel do
+    url "https://downloads.sourceforge.net/project/unfs3/unfs3/0.9.22/unfs3-0.9.22.tar.gz"
+    sha256 "482222cae541172c155cd5dc9c2199763a6454b0c5c0619102d8143bb19fdf1c"
+  end
 
   head do
     url "http://svn.code.sf.net/p/unfs3/code/trunk/"
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
+    depends_on "autoconf"
+    depends_on "automake"
   end
 
   def install
     ENV.deparallelize # Build is not parallel-safe
-    system "./bootstrap" if build.head?
+    system "./bootstrap"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
